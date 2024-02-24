7b3295923df812dc4ba9f6831de188257bf0a3a7
Xiaoyu Hou <xiaoyuhou@Xiaoyus-MBP-3.localdomain>
2024-02-24 12:22:54 -0800

    add 3D Photo Dome Support

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ import {
   Texture,
   PhotoDome,
   Animation,
-  UniversalCamera
+  UniversalCamera,
 } from "@babylonjs/core";
+
 import "@babylonjs/loaders";
 import "@babylonjs/core/Particles/particleSystemComponent";
 import "@babylonjs/core/Particles/webgl2ParticleSystem";
@@ -17,6 +18,7 @@ import {
   Button,
   Control
 } from "@babylonjs/gui";
+import * as BABYLON from "@babylonjs/core";
 
 import { WebXRDefaultExperience } from '@babylonjs/core/XR/webXRDefaultExperience.js'
 
@@ -29,8 +31,14 @@ import '@babylonjs/loaders/glTF'
 //  FragmentOutput[FragmentOutputBlock] is not connected and is not optional.
 import '@babylonjs/core/Materials/Node/Blocks'
 
-import borghese from "./assets/forFacebook-8K-LA.jpg";
-import alps from "./assets/italy.jpg";
+import losAngeles from "./assets/forFacebook-8K-LA.jpg";
+import athens from "./assets/Athens-8K.jpg";
+import lakeTahao from "./assets/LakeTahao-10K.jpg";
+import romeChruch from "./assets/bigchurch.jpg";
+import athen3d from "./assets/Athen-3D.jpg";
+import dubai from "./assets/Dubai.jpg";
+import inTandem from "./assets/in_tandem_3d360.mp4";
+
 
 const canvas = document.getElementById("renderCanvas");
 const engine = new Engine(canvas, true);
@@ -38,7 +46,7 @@ const engine = new Engine(canvas, true);
 const scene = new Scene(engine);
 //var xrHelper = scene.createDefaultXRExperienceAsync();
 var vrHelper = scene.createDefaultVRExperience();
-new HemisphericLight("hemiLight", new Vector3(0, 1, 0));
+//new HemisphericLight("hemiLight", new Vector3(0, 1, 0));
 
 const camera = new UniversalCamera(
   "camera",
@@ -56,182 +64,227 @@ window.addEventListener("resize", () => {
 
 
 // Create the PhotoDome
-let dome = new PhotoDome(
+var dome = new PhotoDome(
   "sphere",
-  alps,
+  athen3d,
   {
     resolution: 64,
     size: 1000,
+    //halfDomeMode: true,
     useDirectMapping: false
   },
   scene
 );
+dome.imageMode = BABYLON.PhotoDome.MODE_TOPBOTTOM;
 
-// Create a GUI texture
-const advancedTexture = AdvancedDynamicTexture.CreateFullscreenUI(
-  "UI"
-);
+//dome.imageMode = PhotoDome.MODE_TOPBOTTOM = 1;
 
-// Create a stack panel to hold the buttons
-const stackPanel = new StackPanel();
-stackPanel.width = "220px";
-stackPanel.horizontalAlignment =
-  Control.HORIZONTAL_ALIGNMENT_RIGHT;
-stackPanel.verticalAlignment =
-  Control.VERTICAL_ALIGNMENT_CENTER;
-advancedTexture.addControl(stackPanel);
+// // Create a GUI texture
+// const advancedTexture = AdvancedDynamicTexture.CreateFullscreenUI(
+//   "UI"
+// );
 
-// Create the buttons
-const button1 = Button.CreateSimpleButton(
-  "button1",
-  "Alps"
-);
-button1.width = "100px";
-button1.height = "40px";
-button1.color = "white";
-button1.thickness = 2;
-button1.background = "green";
-button1.onPointerUpObservable.add(() => {
-  button1.thickness = 2;
-  button2.thickness = 0;
-  transition(alps);
-});
-stackPanel.addControl(button1);
+// // Create a stack panel to hold the buttons
+// const stackPanel = new StackPanel();
+// stackPanel.width = "300px";
+// stackPanel.horizontalAlignment =
+//   Control.HORIZONTAL_ALIGNMENT_RIGHT;
+// stackPanel.verticalAlignment =
+//   Control.VERTICAL_ALIGNMENT_CENTER;
+// advancedTexture.addControl(stackPanel);
 
-const button2 = Button.CreateSimpleButton(
-  "button2",
-  "Gardens"
-);
-button2.width = "100px";
-button2.height = "40px";
-button2.thickness = 0;
-button2.color = "white";
-button2.background = "red";
-button2.onPointerUpObservable.add(() => {
-  button1.thickness = 0;
-  button2.thickness = 2;
-  transition(borghese);
-});
-stackPanel.addControl(button2);
+// // Create the buttons
+// const button1 = Button.CreateSimpleButton(
+//   "button1",
+//   "Los Angeles"
+// );
+// button1.width = "200px";
+// button1.height = "100px";
+// button1.paddingBottom ="30px";
+// button1.cornerRadius="10";
+// button1.shadowColor="black";
+// button1.shadowOffsetX="2";
+// button1.shadowOffsetY="2";
+// button1.shadowBlur="30";
+// button1.color = "white";
+// button1.thickness = 2;
+// button1.background = "gray";
+// button1.alpha = "0.5";
+// button1.onPointerUpObservable.add(() => {
+//   button1.thickness = 2;
+//   button2.thickness = 0;
+//   transition(losAngeles);
+// });
+// stackPanel.addControl(button1);
 
-const transition = (image) => {
-  let anim = scene.beginDirectAnimation(
-    dome.mesh,
-    [fadeOutAnimation],
-    0,
-    120,
-    false
-  );
-  anim.onAnimationEnd = () => loadNewTexture(image);
-};
+// const button2 = Button.CreateSimpleButton(
+//   "button2",
+//   "Athens, Greece"
+// );
+// button2.width = "200px";
+// button2.height = "100px";
+// button2.paddingBottom ="30px";
+// button2.cornerRadius="10";
+// button2.shadowColor="black";
+// button2.shadowOffsetX="2";
+// button2.shadowOffsetY="2";
+// button2.shadowBlur="30";
+// button2.color = "white";
+// button2.thickness = 0;
+// button2.background = "gray";
+// button2.alpha = "0.5";
+// button2.onPointerUpObservable.add(() => {
+//   button1.thickness = 0;
+//   button2.thickness = 2;
+//   transition(athens);
+// });
+// stackPanel.addControl(button2);
 
-const loadNewTexture = (image) => {
-  const newTexture = new Texture(image, scene);
-  newTexture.onLoadObservable.add(() => {
-    dome.dispose();
+// const button3 = Button.CreateSimpleButton(
+//   "button3",
+//   "3D 360 Photo"
+// );
+// button3.width = "200px";
+// button3.height = "100px";
+// button3.paddingBottom ="30px";
+// button3.cornerRadius="10";
+// button3.shadowColor="black";
+// button3.shadowOffsetX="2";
+// button3.shadowOffsetY="2";
+// button3.shadowBlur="30";
+// button3.color = "white";
+// button3.thickness = 0;
+// button3.background = "gray";
+// button3.alpha = "0.5";
+// button3.onPointerUpObservable.add(() => {
+//   button1.thickness = 0;
+//   button2.thickness = 0;
+//   button3.thickness = 2;
+//   transition(athen3d);
+// });
+// stackPanel.addControl(button3);
 
-    // Create a new dome with the new texture
-    dome = new PhotoDome(
-      "sphere",
-      image,
-      {
-        resolution: 64,
-        size: 1000,
-        useDirectMapping: false
-      },
-      scene
-    );
-    dome.mesh.material.alpha = 0;
-    scene.beginDirectAnimation(
-      dome.mesh,
-      [fadeInAnimation],
-      0,
-      120,
-      false
-    );
-  });
-};
+// const transition = (image) => {
+//   let anim = scene.beginDirectAnimation(
+//     dome.mesh,
+//     [fadeOutAnimation],
+//     0,
+//     120,
+//     false
+//   );
+//   anim.onAnimationEnd = () => loadNewTexture(image);
+// };
 
-const fadeOutAnimation = new Animation(
-  "fadeOut",
-  "material.alpha",
-  40,
-  Animation.ANIMATIONTYPE_FLOAT,
-  Animation.ANIMATIONLOOPMODE_CONSTANT
-);
+// const loadNewTexture = (image) => {
+//   const newTexture = new Texture(image, scene);
+//   newTexture.onLoadObservable.add(() => {
+//     dome.dispose();
 
-fadeOutAnimation.setKeys([
-  { frame: 0, value: 1 },
-  { frame: 120, value: 0 }
-]);
+//     // Create a new dome with the new texture
+//     dome = new PhotoDome(
+//       "sphere",
+//       image,
+//       {
+//         resolution: 128,
+//         size: 1000,
+//         useDirectMapping: false
+//       },
+//       scene
+//     );
+    
+//     dome.mesh.material.alpha = 0;
+//     dome.imageMode = PhotoDome.MODE_TOPBOTTOM;
+//     scene.beginDirectAnimation(
+//       dome.mesh,
+//       [fadeInAnimation],
+//       0,
+//       120,
+//       false
+//     );
+//   });
+// };
 
-const fadeInAnimation = new Animation(
-  "fadeIn",
-  "material.alpha",
-  40,
-  Animation.ANIMATIONTYPE_FLOAT,
-  Animation.ANIMATIONLOOPMODE_CONSTANT
-);
+// const fadeOutAnimation = new Animation(
+//   "fadeOut",
+//   "material.alpha",
+//   40,
+//   Animation.ANIMATIONTYPE_FLOAT,
+//   Animation.ANIMATIONLOOPMODE_CONSTANT
+// );
 
-fadeInAnimation.setKeys([
-  { frame: 0, value: 0 },
-  { frame: 120, value: 1 }
-]);
+// fadeOutAnimation.setKeys([
+//   { frame: 0, value: 1 },
+//   { frame: 120, value: 0 }
+// ]);
+
+// const fadeInAnimation = new Animation(
+//   "fadeIn",
+//   "material.alpha",
+//   40,
+//   Animation.ANIMATIONTYPE_FLOAT,
+//   Animation.ANIMATIONLOOPMODE_CONSTANT
+// );
+
+// fadeInAnimation.setKeys([
+//   { frame: 0, value: 0 },
+//   { frame: 120, value: 1 }
+// ]);
 // Assume `scene` is your Babylon.js scene
 
 // Create the zoom in button
-var zoomInButton = Button.CreateSimpleButton(
-  "zoomInButton",
-  "Zoom In"
-);
-zoomInButton.paddingTopInPixels = 50;
-zoomInButton.width = "100px";
-zoomInButton.height = "90px";
-zoomInButton.color = "white";
-zoomInButton.background = "blue";
-zoomInButton.onPointerUpObservable.add(() => {
-  camera.fov = Math.max(0.1, camera.fov - 0.1);
-});
-stackPanel.addControl(zoomInButton);
+// var zoomInButton = Button.CreateSimpleButton(
+//   "zoomInButton",
+//   "Zoom In"
+// );
+// zoomInButton.paddingTopInPixels = 50;
+// zoomInButton.width = "100px";
+// zoomInButton.height = "90px";
+// zoomInButton.color = "white";
+// zoomInButton.background = "blue";
+// zoomInButton.onPointerUpObservable.add(() => {
+//   camera.fov = Math.max(0.1, camera.fov - 0.1);
+// });
+// stackPanel.addControl(zoomInButton);
 
 // Create the zoom out button
-var zoomOutButton = Button.CreateSimpleButton(
-  "zoomOutButton",
-  "Zoom Out"
-);
-zoomOutButton.width = "100px";
-zoomOutButton.height = "40px";
-zoomOutButton.color = "white";
-zoomOutButton.background = "blue";
-zoomOutButton.onPointerUpObservable.add(() => {
-  camera.fov = Math.min(Math.PI / 2, camera.fov + 0.1);
-});
-stackPanel.addControl(zoomOutButton);
+// var zoomOutButton = Button.CreateSimpleButton(
+//   "zoomOutButton",
+//   "Zoom Out"
+// );
+// zoomOutButton.width = "100px";
+// zoomOutButton.height = "40px";
+// zoomOutButton.color = "white";
+// zoomOutButton.background = "blue";
+// zoomOutButton.onPointerUpObservable.add(() => {
+//   camera.fov = Math.min(Math.PI / 2, camera.fov + 0.1);
+// });
+// stackPanel.addControl(zoomOutButton);
 
 // Create a rotation animation
-let isRotationPlaying = true;
-let rotationSpeed = 0.00001; // Adjust as needed for faster/slower rotation
+// let isRotationPlaying = true;
+// let rotationSpeed = 0.00001; // Adjust as needed for faster/slower rotation
 
 // Create the rotation button
-const rotationButton = Button.CreateSimpleButton(
-  "rotationButton",
-  "Pause"
-);
-rotationButton.width = "100px";
-rotationButton.height = "40px";
-rotationButton.color = "white";
-rotationButton.background = "purple";
-rotationButton.onPointerUpObservable.add(() => {
-  isRotationPlaying = !isRotationPlaying;
-  rotationButton.textBlock.text = isRotationPlaying
-    ? "Pause"
-    : "Play";
-});
-stackPanel.addControl(rotationButton);
+// const rotationButton = Button.CreateSimpleButton(
+//   "rotationButton",
+//   "Pause"
+// );
+// rotationButton.width = "100px";
+// rotationButton.height = "40px";
+// rotationButton.color = "white";
+// rotationButton.background = "purple";
+// rotationButton.onPointerUpObservable.add(() => {
+//   isRotationPlaying = !isRotationPlaying;
+//   rotationButton.textBlock.text = isRotationPlaying
+//     ? "Pause"
+//     : "Play";
+// });
+// stackPanel.addControl(rotationButton);
 
 // Update camera rotation in the render loop based on isRotationPlaying
-scene.onBeforeRenderObservable.add(() => {
-  if (isRotationPlaying) {
-    camera.rotation.y += rotationSpeed;
-  }
-});
+// scene.onBeforeRenderObservable.add(() => {
+//   //dome.imageMode = PhotoDome.MODE_SIDEBYSIDE;
+//   // if (isRotationPlaying) {
+//   //   camera.rotation.y += rotationSpeed;
+//   // }
+// });
